4a4b9676199f0f73b05104a39a5f0e457f4ca4d7
Daniil-Ctrl-c <daniilkolomytov@gmail.com>
2025-05-28 23:29:36 +0300

    model filter

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -3,32 +3,22 @@ from recipes.models import Ingredient, Recipe, Tag
 
 
 class RecipeFilter(filters.FilterSet):
-    # IN-фильтр по slug-ам тегов
-    tags = filters.BaseInFilter(
-        field_name="tags__slug",
-        lookup_expr="in"
-    )
+    tags = filters.BaseInFilter(field_name="tags__slug", lookup_expr="in")
     author = filters.NumberFilter(field_name="author__id")
     is_favorited = filters.BooleanFilter(method="filter_favorited")
     is_in_shopping_cart = filters.BooleanFilter(method="filter_shopping_cart")
 
     class Meta:
         model = Recipe
-        fields = (
-            "tags",             # теперь принимает список ?tags=egg,milk,...
-            "author",
-            "is_favorited",
-            "is_in_shopping_cart",
-        )
+        fields = ("tags", "author", "is_favorited", "is_in_shopping_cart")
 
     def filter_favorited(self, queryset, name, value):
         user = getattr(self.request, "user", None)
         if not user or user.is_anonymous:
             return queryset.none() if value else queryset
         qs = (
-            queryset.filter(favorited_by__user=user)
-            if value
-            else queryset.exclude(favorited_by__user=user)
+            queryset.filter(favorites__user=user) if value
+            else queryset.exclude(favorites__user=user)
         )
         return qs.distinct()
 
@@ -37,18 +27,14 @@ class RecipeFilter(filters.FilterSet):
         if not user or user.is_anonymous:
             return queryset.none() if value else queryset
         qs = (
-            queryset.filter(shopping_carts__user=user)
-            if value
-            else queryset.exclude(shopping_carts__user=user)
+            queryset.filter(shoppingcarts__user=user) if value
+            else queryset.exclude(shoppingcarts__user=user)
         )
         return qs.distinct()
 
 
 class IngredientFilter(filters.FilterSet):
-    name = filters.CharFilter(
-        field_name="name",
-        lookup_expr="istartswith"
-    )
+    name = filters.CharFilter(field_name="name", lookup_expr="istartswith")
 
     class Meta:
         model = Ingredient
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -4,42 +4,27 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(
-        max_length=200, unique=True,
-        verbose_name="Название тега"
-    )
-    color = models.CharField(
-        max_length=7,
-        verbose_name="Цвет (HEX)"
-    )
-    slug = models.SlugField(
-        unique=True,
-        verbose_name="Слаг"
-    )
+    name = models.CharField(max_length=200, unique=True, verbose_name="Название тега")
+    color = models.CharField(max_length=7, verbose_name="Цвет (HEX)")
+    slug = models.SlugField(unique=True, verbose_name="Слаг")
 
     class Meta:
         verbose_name = "Тег"
         verbose_name_plural = "Теги"
-        ordering = ["name"]  # упорядочиваем по имени
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
 
 
 class Ingredient(models.Model):
-    name = models.CharField(
-        max_length=200, unique=True,
-        verbose_name="Название ингредиента"
-    )
-    measurement_unit = models.CharField(
-        max_length=200,
-        verbose_name="Единица измерения"
-    )
+    name = models.CharField(max_length=200, unique=True, verbose_name="Название ингредиента")
+    measurement_unit = models.CharField(max_length=200, verbose_name="Единица измерения")
 
     class Meta:
         verbose_name = "Ингредиент"
         verbose_name_plural = "Ингредиенты"
-        ordering = ["name"]  # упорядочиваем по имени
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
@@ -52,28 +37,16 @@ class Recipe(models.Model):
         related_name="recipes",
         verbose_name="Автор"
     )
-    name = models.CharField(
-        max_length=200,
-        verbose_name="Название"
-    )
-    image = models.ImageField(
-        upload_to="recipes/images/",
-        verbose_name="Изображение"
-    )
-    text = models.TextField(
-        verbose_name="Описание"
-    )
+    name = models.CharField(max_length=200, verbose_name="Название")
+    image = models.ImageField(upload_to="recipes/images/", verbose_name="Изображение")
+    text = models.TextField(verbose_name="Описание")
     cooking_time = models.PositiveSmallIntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(1440)],
         verbose_name="Время приготовления (мин)"
     )
-    tags = models.ManyToManyField(
-        Tag,
-        related_name="recipes",
-        verbose_name="Теги"
-    )
+    tags = models.ManyToManyField(Tag, related_name="recipes", verbose_name="Теги")
     ingredients = models.ManyToManyField(
-        "Ingredient",
+        Ingredient,
         through="IngredientInRecipe",
         related_name="recipes",
         verbose_name="Ингредиенты"
@@ -82,7 +55,7 @@ class Recipe(models.Model):
     class Meta:
         verbose_name = "Рецепт"
         verbose_name_plural = "Рецепты"
-        ordering = ["-id"]  # надо для стабильной пагинации
+        ordering = ["-id"]
 
     def __str__(self):
         return self.name
@@ -101,10 +74,7 @@ class IngredientInRecipe(models.Model):
         related_name="ingredientinrecipe_set",
         verbose_name="Ингредиент"
     )
-    amount = models.PositiveSmallIntegerField(
-        validators=[MinValueValidator(1)],
-        verbose_name="Количество"
-    )
+    amount = models.PositiveSmallIntegerField(validators=[MinValueValidator(1)], verbose_name="Количество")
 
     class Meta:
         verbose_name = "Ингредиент в рецепте"
@@ -116,14 +86,11 @@ class IngredientInRecipe(models.Model):
 
 
 class RecipeRelation(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        verbose_name="Пользователь"
-    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Пользователь")
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
+        related_name="%(class)ss",
         verbose_name="Рецепт"
     )
 
