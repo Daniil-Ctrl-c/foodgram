4ac270ba439c734e49cbee02ec9d21ea76ff59fd
Daniil-Ctrl-c <daniilkolomytov@gmail.com>
2025-07-10 19:58:09 +0300

    ревью

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -3,52 +3,54 @@ from recipes.models import Ingredient, Recipe
 
 
 class RecipeFilter(filters.FilterSet):
+    """
+    Фильтры для рецептов:
+    - tags: список slug’ов тегов
+    - author: id автора
+    - is_favorited / is_in_shopping_cart: булевы флаги
+    """
+
     tags = filters.BaseInFilter(field_name="tags__slug", lookup_expr="in")
     author = filters.NumberFilter(field_name="author__id")
     is_favorited = filters.BooleanFilter(method="filter_favorited")
     is_in_shopping_cart = filters.BooleanFilter(
-        method="filter_shopping_cart",
+        method="filter_shopping_cart"
     )
 
     class Meta:
         model = Recipe
         fields = ("tags", "author", "is_favorited", "is_in_shopping_cart")
 
-    # --- избранное --------------------------------------------------------
     def filter_favorited(self, queryset, name, value):
-        """True  -> вернуть рецепты, которые пользователь добавил в избранное
-        false -> исключить такие рецепты
-        """
         user = getattr(self.request, "user", None)
         if user is None or user.is_anonymous:
-            # гость → избранных/корзины быть не может
             return queryset.none() if value else queryset
 
         lookup = {"favorite__user": user}
-        return (
+        qs = (
             queryset.filter(**lookup)
             if value
             else queryset.exclude(**lookup)
-        ).distinct()
+        )
+        return qs.distinct()
 
-    # --- корзина ----------------------------------------------------------
     def filter_shopping_cart(self, queryset, name, value):
-        """True  -> рецепты, находящиеся в корзине пользователя
-        false -> рецепты, не находящиеся в корзине
-        """
         user = getattr(self.request, "user", None)
         if user is None or user.is_anonymous:
             return queryset.none() if value else queryset
 
         lookup = {"shoppingcart__user": user}
-        return (
+        qs = (
             queryset.filter(**lookup)
             if value
             else queryset.exclude(**lookup)
-        ).distinct()
+        )
+        return qs.distinct()
 
 
 class IngredientFilter(filters.FilterSet):
+    """Поиск ингредиента по названию, beginnt mit… (istartswith)."""
+
     name = filters.CharFilter(field_name="name", lookup_expr="istartswith")
 
     class Meta:
